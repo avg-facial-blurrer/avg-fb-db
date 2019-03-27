1a9fad1f755e1ae5d210589f187b566da9c7212b
Meron Brouwer <meron.brouwer@gmail.com>
2019-03-27 20:27:36 +0100

    Initial commit

---
+++ DMLScript.sql
@@ -0,0 +1,11 @@
+/*
+DML script for Mysql DB
+
+from wikipedia:
+In SQL, the data manipulation language comprises the SQL-data change statements, which modify stored data but not the schema or database objects. 
+Manipulation of persistent database objects, e.g., tables or stored procedures, via the SQL schema statements, rather than the data stored within them, is considered to be part of a separate data definition language (DDL). 
+In SQL these two categories are similar in their detailed syntax, data types, expressions etc., but distinct in their overall function.
+The SQL-data change statements are a subset of the SQL-data statements; this also contains the SELECT query statement, which strictly speaking is part of the DQL, not the DML. 
+In common practice though, this distinction is not made and SELECT is widely considered to be part of DML,so the DML consists of all SQL-data statements, not only the SQL-data change statements. 
+The SELECT ... INTO ... form combines both selection and manipulation, and thus is strictly considered to be DML because it manipulates (i.e. modifies) data. 
+*/
